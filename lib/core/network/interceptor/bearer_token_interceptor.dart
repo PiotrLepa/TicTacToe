c06de22a473ddc029928359b4b13a9cd5bac6e7c
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-02-17 23:48:39 +0100

    implement oauth tokens storage

--- lib/core/network/interceptor/bearer_token_interceptor.dart
+++ lib/core/network/interceptor/bearer_token_interceptor.dart
@@ -1,11 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:tictactoe/core/network/network_constant.dart';
+import 'package:tictactoe/core/storage/oauth_tokens_storage.dart';
 
 class BearerTokenInterceptor extends InterceptorsWrapper {
+  final _oauthTokensStorage = OauthTokensStorage();
+
   @override
-  Future onRequest(RequestOptions options) {
-    final accessToken =
-        "a4391f14-16ee-4df5-bcc1-312a9bd10210"; // TODO get from prefs
+  Future onRequest(RequestOptions options) async {
+    final accessToken = await _oauthTokensStorage.accessToken;
     final headers = options.headers;
 
     if (accessToken != null && _isRequestSecure(headers)) {
@@ -19,14 +21,10 @@ class BearerTokenInterceptor extends InterceptorsWrapper {
   static bool _isRequestSecure(Map<String, dynamic> headers) =>
       headers[securedHeader] == true;
 
-  void _addAuthorizationHeader(
-    Map<String, dynamic> headers,
-    String accessToken,
-  ) =>
+  void _addAuthorizationHeader(Map<String, dynamic> headers,
+      String accessToken,) =>
       headers[authorizationHeader] = '$bearerPrefix $accessToken';
 
-  void _removeInternalSecuredHeader(
-    Map<String, dynamic> headers,
-  ) =>
+  void _removeInternalSecuredHeader(Map<String, dynamic> headers,) =>
       headers..remove(securedHeader);
 }
