561c9b0a11e1d0b8f9b077a0f5c205f80da16807
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-03-10 20:04:55 +0100

    fix convert enum to string

--- lib/data/service/network_service.dart
+++ lib/data/service/network_service.dart
@@ -5,6 +5,7 @@ import 'package:tictactoe/core/data/network/network_constant.dart';
 import 'package:tictactoe/core/data/serializer/response_converter.dart';
 import 'package:tictactoe/core/data/service/base_network_service.dart';
 import 'package:tictactoe/core/injection/injection_names.dart';
+import 'package:tictactoe/core/util/enum_helper.dart';
 import 'package:tictactoe/data/model/common/difficulty_level/difficulty_level_model.dart';
 import 'package:tictactoe/data/model/game_response/game_response_model.dart';
 import 'package:tictactoe/data/model/login_request/login_request_model.dart';
@@ -13,10 +14,14 @@ import 'package:tictactoe/data/model/login_response/login_response_model.dart';
 @injectable
 class NetworkService extends BaseNetworkService {
   NetworkService(
-      @Named(defaultNetworkClient) Dio dio, ResponseConverter responseConverter)
-      : super(dio, responseConverter);
+    @Named(defaultNetworkClient) Dio dio,
+    ResponseConverter responseConverter,
+  ) : super(dio, responseConverter);
 
-  Future<Response<LoginResponseModel>> login(LoginRequestModel request) => post(
+  Future<Response<LoginResponseModel>> login(
+    LoginRequestModel request,
+  ) =>
+      post(
         "/oauth/token",
         data: request,
         contentType: Headers.formUrlEncodedContentType,
@@ -29,10 +34,7 @@ class NetworkService extends BaseNetworkService {
       post(
         "/game/create",
         queryParameters: {
-          "difficulty_level": difficultyLevel
-              .toString()
-              .split(".")[1]
-              .toUpperCase(), // TODO server cannot convert lower case enum
+          "difficulty_level": enumToString(difficultyLevel),
         },
         secured: true,
       );
