c040631d7266c95dfc203771b1109eecf7532cba
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-05-02 13:19:26 +0200

    ping to web socket perodically to keep it alive

--- lib/data/service/stomp_service.dart
+++ lib/data/service/stomp_service.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
 import 'package:stomp_dart_client/stomp.dart';
 import 'package:stomp_dart_client/stomp_config.dart';
@@ -13,6 +11,7 @@ import 'package:tictactoe/data/model/multiplayer_game_response/multiplayer_game_
 
 @lazySingleton
 class StompService {
+  static final _multiplayerSocket = webSocketUrl + "/multiplayer-socket";
   final ModelDecoder _modelDecoder;
 
   StompService(
@@ -21,59 +20,43 @@ class StompService {
 
   Stream<MultiplayerGameResponseModel> getMultiplayerGame(int gameId) {
     final _controller = StreamController<MultiplayerGameResponseModel>();
-    _connectToSocket(
-      onConnected: (client) {
-        client.subscribe(
-          destination: '/game-status/$gameId',
-          callback: (frame) {
-            logger.d("game status: " + frame.body);
-            final model = _modelDecoder
-                .decode<MultiplayerGameResponseModel>(jsonDecode(frame.body));
-            _controller.add(model);
-          },
-        );
-      },
-      onError: () {},
-    );
+    _connectToSocket().then((client) {
+      _pingPeriodicallyToSocket(client);
+      client.subscribe(
+        destination: '/game-status/$gameId',
+        callback: (frame) {
+          logger.d("game status: " + frame.body);
+          final model = _modelDecoder
+              .decode<MultiplayerGameResponseModel>(jsonDecode(frame.body));
+          _controller.add(model);
+        },
+      );
+    });
     return _controller.stream;
   }
 
-  StompClient _connectToSocket({
-    @required Function(StompClient) onConnected,
-    @required VoidCallback onError,
-  }) =>
-      StompClient(
-        config: StompConfig(
-          url: webSocketUrl + "/multiplayer-socket",
-          onConnect: (client, frame) {
-            logger.d("socket connected" + frame.body);
-            onConnected(client);
-          },
-          onStompError: (error) {
-            logger.d("stomp error" + error.toString());
-            onError();
-          },
-          onDebugMessage: (message) {
-            logger.d("onDebugMessage" + message);
-          },
-          onWebSocketDone: () {
-            logger.d("onWebSocketDone");
-          },
-          onDisconnect: (stompFrame) {
-            logger.d("onDisconnect");
-          },
-          onUnhandledFrame: (stompFrame) {
-            logger.d("onUnhandledFrame");
-          },
-          onUnhandledMessage: (stompFrame) {
-            logger.d("onUnhandledMessage");
-          },
-          onUnhandledReceipt: (stompFrame) {
-            logger.d("onUnhandledReceipt");
-          },
-          onWebSocketError: (stompFrame) {
-            logger.d("onWebSocketError");
-          },
-        ),
-      )..activate();
+  Future<StompClient> _connectToSocket() {
+    final completer = Completer<StompClient>();
+    StompClient(
+      config: StompConfig(
+        url: _multiplayerSocket,
+        onConnect: (client, frame) {
+          completer.complete(client);
+        },
+      ),
+    ).activate();
+
+    return completer.future;
+  }
+
+  void _pingPeriodicallyToSocket(StompClient client) {
+    // client loses connection after 1 minute of inactivity
+    // so we need to periodically send ping to web socket to keep it alive
+    Timer.periodic(Duration(seconds: 30), (timer) {
+      client.send(
+        destination: _multiplayerSocket,
+        body: 'Ping $_multiplayerSocket',
+      );
+    });
+  }
 }
